32a9f9784efafd15e9d02d4cbc301e5957fdf32c
nthBOT <89666509+nth-bot@users.noreply.github.com>
2021-08-29 18:02:18 +0200

    Add files via upload

--- chat.js
+++ chat.js
@@ -55,31 +55,31 @@ window.onload = function() {
 
         ui.e(`
 
-        Welcome to Zero's NthBOT, a minimalist & 'not too high' chatbot engine.
+    Welcome to Zero's NthBOT, a minimalist & 'not too high' chatbot engine.
 
-            op    type         description
-            --    ----         -----------
-            
-            #     delimiter    rule
-            <     condition    input
-            >     action       output
-            @     action       selfput
-            *     condition    is in db
-            /     condition    is not in db
-            +     action       add to db
-            -     action       remove from db
-            {}    inline       capture
-            []    inline       insert
-            
-        In the toolbar:
-        - Clear, clears the terminal
-        - List, shows a list of editors saved in the browser's Localstorage
-        - Load, loads an editor from the browser's Localstorage
-        - Save, saves the current editor to the browser's Localstorage
-        - Dump, writes in the editor the current content of the bot's database
-        - Refresh, replaces the bot's database by the current editor
-        - Help, shows this help file in the editor
-        - Doc, opens the NthBOT documentation page on Github
+        op    type         description
+        --    ----         -----------
+        
+        #     delimiter    rule
+        <     condition    input
+        >     action       output
+        @     action       selfput
+        *     condition    is in db
+        /     condition    is not in db
+        +     action       add to db
+        -     action       remove from db
+        {}    inline       capture
+        []    inline       insert
+        
+    In the toolbar:
+    - Clear, clears the terminal
+    - List, shows a list of editors saved in the browser's Localstorage
+    - Load, loads an editor from the browser's Localstorage
+    - Save, saves the current editor to the browser's Localstorage
+    - Dump, writes in the editor the current content of the bot's database
+    - Refresh, replaces the bot's database by the current editor
+    - Help, shows this help file in the editor
+    - Doc, opens the NthBOT documentation page on Github
 
         `);
     });
